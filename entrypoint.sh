e07207fc5cd79fabfb754e90ba87730212348413
runnymamaly <alternative2236@atomicmail.io>
2026-06-07 23:00:58 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
-echo "PATH="/tmp:$PATH"" > /etc/environment
+export PATH="/opt:$PATH"
 if ! command -v xray >/dev/null; then
   7z x -y -bsp0 -bso0 "xray.7z" -o"/tmp"
   chmod +x "/tmp/xray"
+  chmod +x "/opt/command.sh"
 fi
 exec "$@"
