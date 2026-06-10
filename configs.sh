0669c40a6c2b35426b6f1cde1bed8516ff815ae2
runnymamaly <alternative2236@atomicmail.io>
2026-06-11 00:39:29 +0330

    Update configs.sh

--- configs.sh
+++ configs.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
-if [ -z "$(ls -A "data" 2>/dev/null)" ]; then
-  xray uuid >"data/uuid"
-  xray x25519 >"data/keys"
-fi
+
 ID="$(cat "data/uuid")"
 PRIVATE_KEY="$(awk '/PrivateKey/{print $2}' "data/keys")"
 PUBLIC_KEY="$(awk '/Password/{print $3}' "data/keys")"
@@ -13,78 +10,10 @@ if [ -z "${ADDRESS}" ]; then echo "The ADDRESS environment variable must be set!
 if [ "${NETWORK}" = "ws" ]; then
   # echo "vless://${ID}@${ADDRESS}:${PORT}?type=xhttp&security=reality&pbk=${PUBLIC_KEY}&sni=${SNI}&fp=chrome#${ADDRESS}" | qrencode -t ansiutf8
   echo "vless://${ID}@${ADDRESS}:${PORT}?encryption=none&security=tls&sni=${ADDRESS}&fp=chrome&alpn=http%2F1.1&insecure=0&allowInsecure=0&type=ws#${ADDRESS}" | qrencode -t ansiutf8
-  cat >"/etc/xray.json" <<-EOF
-		{
-			"log": {
-				"loglevel": "warning"
-			},
-			"inbounds": [
-				{
-					"port": 443,
-					"protocol": "vless",
-					"settings": {
-						"clients": [
-							{
-								"id": "${ID}"
-							}
-						],
-						"decryption": "none"
-					},
-					"streamSettings": {
-						"network": "ws"
-					}
-				}
-			],
-			"outbounds": [
-				{
-					"protocol": "freedom"
-				}
-			]
-		}
-	EOF
+  
 elif [ "${NETWORK}" = "tcp" ]; then
   echo "vless://${ID}@${ADDRESS}:${PORT}?type=tcp&flow=xtls-rprx-vision&security=reality&pbk=${PUBLIC_KEY}&sni=${SNI}&fp=chrome#${ADDRESS}" | qrencode -t ansiutf8
-  cat >"/etc/xray.json" <<-EOF
-		{
-			"log": {
-				"loglevel": "warning"
-			},
-			"inbounds": [
-				{
-					"port": 443,
-					"protocol": "vless",
-					"settings": {
-						"clients": [
-							{
-								"flow": "xtls-rprx-vision",
-								"id": "${ID}"
-							}
-						],
-						"decryption": "none"
-					},
-					"streamSettings": {
-						"network": "tcp",
-						"security": "reality",
-						"realitySettings": {
-							"dest": "${SNI}:443",
-							"serverNames": [
-								"${SNI}"
-							],
-						"privateKey": "${PRIVATE_KEY}",
-						"shortIds": [
-							""
-							]
-						}
-					}
-				}
-			],
-			"outbounds": [
-				{
-					"protocol": "freedom"
-				}
-			]
-		}
-	EOF
+  
 else
   echo 'The NETWORK environment variable must be set to "tcp" or "xhttp"!' >&2
   exit 1
